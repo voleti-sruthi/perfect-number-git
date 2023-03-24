b4b85506717d2a1dfc198afccff206a2ec94fd88
voleti-sruthi <sruthi.lakshmi@m2pfintech.com>
2023-03-24 06:27:15 +0530

    Add. Included the logic to print perfect numbers upto a limit

--- src/PerfectNumbers.java
+++ src/PerfectNumbers.java
@@ -1,5 +1,43 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PerfectNumbers {
+    public static ArrayList<Integer> checkPerfectNumbersUntil(int perfectNumbersUntil){
+
+        ArrayList<Integer> listOfPerfectNumbers = new ArrayList<>();
+
+        for( int numberToCheck = 1 ; numberToCheck <= perfectNumbersUntil ; numberToCheck++ ){
+
+            int sum = 0;
+
+            for( int iterator = 1 ; iterator < numberToCheck ; iterator++ ){
+
+                if( numberToCheck % iterator == 0 ){
+
+                    sum = sum + iterator;
+
+                }
+
+            }
+
+            if( sum == numberToCheck ){
+
+                listOfPerfectNumbers.add(numberToCheck);
+
+            }
+
+        }
+
+        return listOfPerfectNumbers;
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int perfectNumbersUntil = scanner.nextInt();
+
+        System.out.println(checkPerfectNumbersUntil(perfectNumbersUntil));
+
     }
 }
